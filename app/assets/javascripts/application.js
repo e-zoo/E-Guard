736161c054510c6f6790d5d1067f8bef03cecd4f
infinitydev4 <infinitydev4@gmail.com>
2018-10-19 10:00:26 -0700

    initializers/config.assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require vendors/bootstrap.min
+//= require vendors/bootstrap
 //= require custom
 //= require vendors/jquery.min
 //= require vendors/respond.min
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,4 +15,6 @@ Rails.application.config.assets.paths << Rails.root.join('vendors')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( bootstrap.js bootstrap.bundle.min.js bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( jquery.min.js jqBootstrapValidation.js  )
 
